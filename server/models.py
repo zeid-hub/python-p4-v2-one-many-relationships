c62ff9d58594ba39fa1a69be2f50b3dd142e7dde
zeid <zeidhassan4@gmail.com>
2024-04-05 05:02:00 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -13,26 +13,37 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Employee(db.Model):
-    __tablename__ = "employees"
+    __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
-    def __repr__(self):
-        return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
+    # Relationship mapping the employee to related reviews
+    reviews = db.relationship('Review', back_populates="employee")
 
+    # Relationship mapping employee to related onboarding
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee')
+
+    def __repr__(self):
+        return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
 class Onboarding(db.Model):
-    __tablename__ = "onboardings"
+    __tablename__ = 'onboardings'
 
     id = db.Column(db.Integer, primary_key=True)
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
-    def __repr__(self):
-        return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
 
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
+
+    def __repr__(self):
+        return f'<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>'
 
 class Review(db.Model):
     __tablename__ = "reviews"
@@ -40,6 +51,13 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+
+    # Foreign key stores the Employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
+
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
--- server/seed.py
+++ server/seed.py
@@ -18,13 +18,20 @@ with app.app_context():
     db.session.commit()
 
     # 1..many relationship between Employee and Review
-    uri_2023 = Review(year=2023, summary="Great web developer!")
-    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work")
-    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches")
-    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker")
+    uri_2023 = Review(year=2023,
+                summary="Great web developer!",
+                employee=uri)
+    tristan_2021 = Review(year=2021,
+                    summary="Good coding skills, often late to work",
+                    employee=tristan)
+    tristan_2022 = Review(year=2022,
+                    summary="Strong coding skills, takes long lunches",
+                    employee=tristan)
+    tristan_2023 = Review(year=2023,
+                    summary="Awesome coding skills, dedicated worker",
+                    employee=tristan)
     db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
     db.session.commit()
-
     # 1..1 relationship between Employee and Onboarding
     uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
     tristan_onboarding = Onboarding(
